35ed58edc6fa18f52fbf5659fc7cd1a334f4b515
Rogério Gomes <165937655+yGanTz@users.noreply.github.com>
2024-10-17 14:43:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let formulario = document.querySelector(".formulario")
+
+function aparecerFormulario(){
+    formulario.style.left = "50%"
+}
+
+function desaparecerFormulario(){
+    formulario.style.left = "-50%"
+}
